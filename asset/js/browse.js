ecb124d7672adf1e75a7b6916cda0d4b4ea25f7e
Kim Nguyen <design@kimisgold.com>
2020-11-11 17:17:44 -0500

    Allow browse control drawer to remain open while paginating on mobile. #33

--- asset/js/browse.js
+++ asset/js/browse.js
@@ -1,16 +1,28 @@
 (function($) {
     $(document).ready(function() {
+      
+        if (localStorage.getItem('are_browse_control_mobile_open')) {
+            toggleBrowseControls();
+        }
+      
         $('.layout-toggle button').click(function() {
             $('.layout-toggle button:disabled').removeAttr('disabled');
             $(this).attr('disabled', true);
             $('.resources').toggleClass('resource-list').toggleClass('resource-grid');
             $('.resources .resource').toggleClass('media-object');
             $('.resource-meta, .resource-image').toggleClass('media-object-section');
+
         });
         
         $('.browse-toggle').click(function() {
-            $('.browse-controls').toggleClass('open closed');
-            $(this).toggleClass('open closed');
+            toggleBrowseControls();
+            localStorage.setItem('are_browse_control_mobile_open', true);
         });
     });
+    
+    var toggleBrowseControls = function() {
+        $('.browse-controls').toggleClass('open closed');
+        $(this).toggleClass('open closed');
+    };
+  
 })(jQuery)
